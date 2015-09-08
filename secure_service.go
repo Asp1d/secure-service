b12ce5c7ce83c95286595e8f3c9859383dcd24a2
Yanis Tsaunya <yanis88@gmail.com>
2015-09-08 14:10:21 +0300

    fix

--- secure_service.go
+++ secure_service.go
@@ -10,7 +10,6 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-    r.
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
